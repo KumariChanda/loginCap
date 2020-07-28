d95ddc700e557eb8d93f1fc8c75b82bc0a4e3bae
vianeyMojuye <vianeytoukam1@gmail.com>
2020-07-28 23:03:42 +0530

    destination

--- src/app/pages/reservation-page/reservation-page.module.ts
+++ src/app/pages/reservation-page/reservation-page.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+
+
 import { IonicModule } from '@ionic/angular';
 
 import { ReservationPagePageRoutingModule } from './reservation-page-routing.module';
@@ -13,6 +16,7 @@ import { ReservationPagePage } from './reservation-page.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    Ng2SearchPipeModule,
     ReservationPagePageRoutingModule
   ],
   declarations: [ReservationPagePage]
--- src/app/pages/reservation-page/reservation-page.page.ts
+++ src/app/pages/reservation-page/reservation-page.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-reservation-page',
@@ -8,7 +9,19 @@ import { Router } from '@angular/router';
 })
 export class ReservationPagePage implements OnInit {
 
-  constructor(private router: Router) { }
+
+
+  // term = '';
+  // filterData = ['item 1','item 2','item 3','item 4']
+
+  list_original = ['item 1','item 2','item 3','item 4'];
+  list_to_show = [];
+  selected_index = -1;
+  show_list = false;
+
+
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
@@ -20,5 +33,46 @@ export class ReservationPagePage implements OnInit {
 
     this.router.navigateByUrl("/dashboard")
   }
+  /////////////////////////////////////////////////////
+  onCancel(val) {
+    this.show_list = false;
+    this.list_to_show = [];
+
+  }
+
+  click_bar() {
+    this.list_to_show = [];
+    this.show_list = true;
+  }
+
+  click_item(val)
+  {
+        // for (let i = 0 ; i < this.list_original.length; i++)
+        // {
+        //     if (this.list_to_show[val].toUpperCase() === this.list_original[i].toUpperCase()) {
+        //         this.selected_index = i;
+        //         break;
+        //     }
+        // }
+        // this.show_list = false;
+  }
+
+  change_query(query)
+  {
+  
+            let k = 0;
+            this.list_to_show = [];
+            for (let i = 0 ; i < this.list_original.length; i++)
+            {
+                if (this.list_original[i].toUpperCase().includes(query.toUpperCase()))
+                {
+                    this.list_to_show[k] =  this.list_original[i];
+                    k += 1;
+                }
+            }
+    
+  }
+
+
 
 }
