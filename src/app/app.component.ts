da178384befcaa13f739b9a42327d9b2b7789cb7
kumari chanda <kumarichanda658@gmail.com>
2020-08-13 16:28:52 +0530

    little

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent {
 
   rootPage:DashboardPage;
   token;
-  testingToken="null";
+  testingToken=123;
   profile='../assets/imgs/profile.png';
   defaultProfile='../assets/imgs/dummyProfile.jpg';
 
